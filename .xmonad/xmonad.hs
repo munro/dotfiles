fff26ae32b3a4979bb9bc3c474509cea46d9a5c8
Munro <munro.github@gmail.com>
2012-03-30 09:32:35 -0700

    * Xmonad — Setup to use node-dzen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,12 +5,11 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.Named
-import XMonad.Layout.ResizableTile 
+import XMonad.Layout.ResizableTile
 import XMonad.ManageHook
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
 import qualified XMonad.StackSet as W
-
 
 -- Program Exceptions
 myManageHook  = composeAll [ resource =? "gitk"  --> doF (W.swapMaster)
@@ -20,55 +19,36 @@ myManageHook  = composeAll [ resource =? "gitk"  --> doF (W.swapMaster)
                            ]
 newManageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
 
--- Xmonad
-main = do
-    hXmobar <- spawnPipe "/usr/bin/xmobar"
-    xmonad =<< xmobar ((withUrgencyHook NoUrgencyHook) defaultConfig
-        { modMask               = mod4Mask 
-        , terminal              = "urxvt -name foobar"
-        , workspaces            = map show [1..6]
-        , normalBorderColor     = colorNormalBorder
-        , focusedBorderColor    = colorFocusedBorder
-        , borderWidth           = 2
-        , layoutHook            = avoidStruts $ wmLayout
-        , manageHook            = newManageHook
-        , logHook               = wmLog hXmobar 
-        } `additionalKeys` wmKeys)
-
 -- Colors
-colorNormalBorder   = "#111111"
-colorFocusedBorder  = "#FFC469"
+colorNormalBorder  = "#111111"
+colorFocusedBorder = "#FFC469"
 
 -- Layout
-wmLayout = layoutHintsToCenter (wmLayoutTall ||| wmLayoutWide ||| Full ||| wmLayoutTiny ||| wmLayoutResize) 
+wmLayout       = layoutHintsToCenter (   wmLayoutTall ||| wmLayoutWide ||| Full
+                                     ||| wmLayoutTiny ||| wmLayoutResize
+                                     )
 
-wmLayoutTall = named "Tall" (Tall 1 (3/100) (6/10))
-wmLayoutWide = named "Wide" (Mirror $ Tall 1 (3/100) (7/10))
-wmLayoutTiny = named "Tiny" (Tall 1 (1/10) (7/10))
-wmLayoutResize = named "Tall" (ResizableTall 1 (3/100) (1/2) [])
+wmLayoutTall   = named "Tall" (Tall          1 (3/100) (6/10))
+wmLayoutWide   = named "Wide" (Mirror $ Tall 1 (3/100) (7/10))
+wmLayoutTiny   = named "Tiny" (Tall          1 (1/10)  (7/10))
+wmLayoutResize = named "Tall" (ResizableTall 1 (3/100) (1/2)  [])
 
 -- Keys
-wmKeys = 
-    [ ((mod4Mask, xK_z), sendMessage MirrorShrink)
-    , ((mod4Mask, xK_a), sendMessage MirrorExpand)
-    ]
+wmKeys = [ ((mod4Mask, xK_z), sendMessage MirrorShrink)
+         , ((mod4Mask, xK_a), sendMessage MirrorExpand)
+         ]
 
--- Logging/Status
-wmLog h = dynamicLogWithPP $ defaultPP
-    { ppOutput              = hPutStrLn h
-    , ppCurrent             = xmobarColor colorFocusedBorder "" . pad
-    , ppVisible             = xmobarColor colorNormalBorder "" . pad
-    , ppHidden              = xmobarColor "#D1C8BC" "" . pad
-    , ppHiddenNoWindows     = xmobarColor "#5C5245" "" . pad
-    , ppUrgent              = xmobarColor "#101010" colorFocusedBorder . xmobarStrip
-    , ppWsSep               = ""
-    , ppSep                 = " : "
-    , ppLayout              = (\x -> case x of
-        "Hinted Tall" -> "#"
-        "Hinted Wide" -> "!"
-        "Hinted Full" -> "*"
-        "Hinted Tiny" -> "@"
-        _             -> "~"
-    )
-    , ppTitle               = xmobarColor colorFocusedBorder "" . shorten 50
-    }
+-- Xmonad
+main = do
+    hNodeDzen <- spawnPipe "/home/ryan/node-dzen"
+    xmonad $ (withUrgencyHook NoUrgencyHook) defaultConfig
+        { modMask            = mod4Mask
+        , terminal           = "urxvt -name foobar"
+        , workspaces         = map show [1..6]
+        , normalBorderColor  = colorNormalBorder
+        , focusedBorderColor = colorFocusedBorder
+        , borderWidth        = 2
+        , layoutHook         = avoidStruts $ wmLayout
+        , manageHook         = newManageHook
+        , logHook            = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn hNodeDzen }
+        } `additionalKeys` wmKeys
