133700ac6cb8f41cb5e68599229d10ec6712575c
Ryan Munro <500774+munro@users.noreply.github.com>
2026-02-11 21:54:21 -0500

    remove old

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-repo="https://github.com/munro/dotfiles.git"
-ssh -T git@github.com </dev/null 2>&1 | grep -q "Hi munro!" && repo="git@github.com:munro/dotfiles.git"
-git clone "$repo" "$HOME/dotfiles"
-cd "$HOME/dotfiles"
-python3 install.py
